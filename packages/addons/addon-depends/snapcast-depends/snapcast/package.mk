8f59d2cbc57f9a21e8485986bcc46b3a86b0b259
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-06-07 18:35:34 +0200

    snapcast: add pipewire support

--- packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
+++ packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
@@ -7,11 +7,19 @@ PKG_SHA256="cb75a71479bf52910bf5f47ae8120ec41c89459b0d77d7cd560e674e437ef050"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/badaix/snapcast"
 PKG_URL="https://github.com/badaix/snapcast/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib asio avahi flac libvorbis popl pulseaudio boost opus"
+PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib asio avahi flac libvorbis popl boost opus"
 PKG_LONGDESC="Synchronous multi-room audio player."
 PKG_BUILD_FLAGS="-sysroot"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_WITH_PULSE=ON"
+if [ "${PULSEAUDIO_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" pulseaudio"
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_WITH_PULSE=ON"
+fi
+
+if [ "${PIPEWIRE_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" pipewire"
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_WITH_PIPEWIRE=ON"
+fi
 
 pre_configure_target() {
   CXXFLAGS="${CXXFLAGS} -pthread \
